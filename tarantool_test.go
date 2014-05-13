730ccf394a835a501f21a82d106eb0b98291e232
Petr Yanovich <fl00r@yandex.ru>
2014-05-13 20:09:02 +0400

    Blocking API

--- tarantool_test.go
+++ tarantool_test.go
@@ -8,14 +8,20 @@ import(
 )
 
 func TestClient(t *testing.T) {
-	server   := "127.0.0.1:3013"
-	spaceNo  := uint32(514)
-	indexNo  := uint32(0)
-	limit    := uint32(10)
-	offset   := uint32(0)
-	key      := []interface{}{ 12 }
-	tuple    := []interface{}{ uint32(12), "hello", "world", "and", "again", "and", "again" }
-	iterator := ""
+	server    := "127.0.0.1:3013"
+	spaceNo   := uint32(514)
+	indexNo   := uint32(0)
+	limit     := uint32(10)
+	offset    := uint32(0)
+	iterator  := ""
+	key       := []interface{}{ 12 }
+	tuple1    := []interface{}{ 12, "Hello World", "Olga" }
+	tuple2    := []interface{}{ 12, "Hello Mars", "Anna" }
+	upd_tuple := []interface{}{ []interface{}{ "=", 1, "Hello Moon" }, []interface{}{ "#", 2, 1 } }
+
+	functionName  := "box.cfg()"
+	functionTuple := []interface{}{ "box.schema.SPACE_ID" }
+
 
 	client, err := Connect(server)
 	if err != nil {
@@ -30,20 +36,63 @@ func TestClient(t *testing.T) {
 	}
 
 	fmt.Println("Ping")
-	fmt.Println("Header", resp.Header)
-	fmt.Println("Body", resp.Body)
-
+	fmt.Println("Code", resp.Code)
+	fmt.Println("Data", resp.Data)
 	fmt.Println("----")
-	fmt.Println("Select")
-	resp, err = client.Select(spaceNo, indexNo, offset, limit, iterator, key)
-	fmt.Println("ERROR", err)
-	fmt.Println("Header", resp.Header)
-	fmt.Println("Body", resp.Body)
 
-	fmt.Println("----")
+	resp, err = client.Insert(spaceNo, tuple1)
 	fmt.Println("Insert")
-	resp, err = client.Insert(spaceNo, tuple)
 	fmt.Println("ERROR", err)
-	fmt.Println("Header", resp.Header)
-	fmt.Println("Body", resp.Body)
+	fmt.Println("Code", resp.Code)
+	fmt.Println("Data", resp.Data)
+	fmt.Println("----")
+
+	resp, err = client.Select(spaceNo, indexNo, offset, limit, iterator, key)
+	fmt.Println("Select")
+	fmt.Println("ERROR", err)
+	fmt.Println("Code", resp.Code)
+	fmt.Println("Data", resp.Data)
+	fmt.Println("----")
+
+	resp, err = client.Replace(spaceNo, tuple2)
+	fmt.Println("Replace")
+	fmt.Println("ERROR", err)
+	fmt.Println("Code", resp.Code)
+	fmt.Println("Data", resp.Data)
+	fmt.Println("----")
+
+	resp, err = client.Select(spaceNo, indexNo, offset, limit, iterator, key)
+	fmt.Println("Select")
+	fmt.Println("ERROR", err)
+	fmt.Println("Code", resp.Code)
+	fmt.Println("Data", resp.Data)
+	fmt.Println("----")
+
+	resp, err = client.Update(spaceNo, indexNo, key, upd_tuple)
+	fmt.Println("Update")
+	fmt.Println("ERROR", err)
+	fmt.Println("Code", resp.Code)
+	fmt.Println("Data", resp.Data)
+	fmt.Println("----")
+
+	resp, err = client.Select(spaceNo, indexNo, offset, limit, iterator, key)
+	fmt.Println("Select")
+	fmt.Println("ERROR", err)
+	fmt.Println("Code", resp.Code)
+	fmt.Println("Data", resp.Data)
+	fmt.Println("----")
+
+	resp, err = client.Delete(spaceNo, indexNo, key)
+	fmt.Println("Delete")
+	fmt.Println("ERROR", err)
+	fmt.Println("Code", resp.Code)
+	fmt.Println("Data", resp.Data)
+	fmt.Println("----")
+
+	resp, err = client.Call(functionName, functionTuple)
+	fmt.Println("Call")
+	fmt.Println("ERROR", err)
+	fmt.Println("Code", resp.Code)
+	fmt.Println("Data", resp.Data)
+	fmt.Println("----")
 }
